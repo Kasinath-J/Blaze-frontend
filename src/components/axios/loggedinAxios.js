c1156989384ce3210018b288ba21ad0be1bb069d
Kasinath J <jrkasinath@gmail.com>
2023-01-21 18:42:25 +0530

    added local url

--- src/components/axios/loggedinAxios.js
+++ src/components/axios/loggedinAxios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // const baseURL = 'https://csbsblaze.pythonanywhere.com/';
 const baseURL = 'https://blaze-backend-test.onrender.com/';
+// const baseURL = 'http://127.0.0.1:8000/';
 
 export const axiosInstance = axios.create({
     baseURL: baseURL,
